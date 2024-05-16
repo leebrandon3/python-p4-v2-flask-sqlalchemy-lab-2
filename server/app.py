933e29fa93e2db8ed0d26575d1cf51628adfed2c
Brandon <brandonslee3@gmail.com>
2024-05-16 13:05:47 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 
-from models import db
+from models import db, Customer, Item, Review
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
